3ef73e0f8114bcc1770b974dd6e428b9cabcdc2e
antonio-cortes-perez <antonio.cortes.perez@gmail.com>
2021-06-24 14:52:37 -0700

    Consolidating classification/training functions in Classifier.cpp

--- Classifier.cpp
+++ Classifier.cpp
@@ -1,6 +1,7 @@
-#include "Classifier.h"
 #include "Matrix.h"
+#include "Mnist.h"
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <utility>
@@ -9,7 +10,13 @@ namespace nn {
 Matrix forward(const Matrix &X, const Matrix &W) { return sigmoid(mult(X, W)); }
 
 Matrix classify(const Matrix &X, const Matrix &W) {
-  return round(forward(X, W));
+  auto Ypred = nn::forward(X, W);
+  auto Labels = createMatrix(Ypred.size(), 1);
+  for (size_t Row = 0; Row < Ypred.size(); ++Row) {
+    Labels[Row][0] = std::max_element(Ypred[Row].begin(), Ypred[Row].end()) -
+                     Ypred[Row].begin();
+  }
+  return Labels;
 }
 
 double loss(const Matrix &X, const Matrix &Y, const Matrix &W) {
@@ -42,4 +49,29 @@ void accuracy(const Matrix &Y, const Matrix &Ypred) {
   std::cout << "Accuracy: " << (CorrectResults * 100 / Y.size()) << "%";
 }
 
+Matrix oneHotEncodeMnist(const Matrix &Labels) {
+  auto Encoded = createMatrix(Labels.size(), 10);
+  for (size_t Row = 0; Row < Labels.size(); ++Row) {
+    Encoded[Row][static_cast<int>(Labels[Row][0])] = 1;
+  }
+  return Encoded;
+}
+
 } // namespace nn
+
+int main() {
+  auto TrainImages = nn::readImageFile("train-images.idx3-ubyte");
+  auto TrainLabels = nn::readLabelFile("train-labels.idx1-ubyte");
+  auto EncodedLabels = nn::oneHotEncodeMnist(TrainLabels);
+  for (size_t Idx = 0; Idx < 5; ++Idx) {
+    nn::printImageAndLabel(TrainImages, TrainLabels, Idx);
+  }
+
+  const auto W = nn::train(TrainImages, EncodedLabels, 20, 0.00001);
+
+  auto TestImages = nn::readImageFile("t10k-images.idx3-ubyte");
+  auto TestLabels = nn::readLabelFile("t10k-labels.idx1-ubyte");
+  nn::accuracy(TestLabels, nn::classify(TestImages, W));
+
+  return 0;
+}
